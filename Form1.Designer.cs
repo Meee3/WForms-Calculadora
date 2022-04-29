ead3879de60c4cb0af05c2ce8ce88efb5b671c2f
Meee3 <estudo.zzz@gmail.com>
2022-04-29 13:17:37 -0300

    WF Calculadora

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WFAula14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtNumeroAleatorio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(97, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(12, 192);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 2;
+            this.btnSomar.Text = "+";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Location = new System.Drawing.Point(12, 221);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtrair.TabIndex = 3;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.Location = new System.Drawing.Point(12, 250);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 4;
+            this.btnDividir.Text = "/";
+            this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.Location = new System.Drawing.Point(12, 279);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 5;
+            this.btnMultiplicar.Text = "*";
+            this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(12, 138);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 23);
+            this.txtNumero.TabIndex = 6;
+            this.txtNumero.TextChanged += new System.EventHandler(this.txtNumero_TextChanged);
+            // 
+            // txtNumeroAleatorio
+            // 
+            this.txtNumeroAleatorio.Enabled = false;
+            this.txtNumeroAleatorio.Location = new System.Drawing.Point(176, 138);
+            this.txtNumeroAleatorio.Name = "txtNumeroAleatorio";
+            this.txtNumeroAleatorio.Size = new System.Drawing.Size(100, 23);
+            this.txtNumeroAleatorio.TabIndex = 7;
+            this.txtNumeroAleatorio.TextChanged += new System.EventHandler(this.txtNumeroAleatorio_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Aleatório";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(160, 228);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 32);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 371);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumeroAleatorio);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtNumeroAleatorio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAula14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //int count = 0;
+        Random random = new Random();
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            int campoNumero = int.Parse(txtNumero.Text);
+            //int campoNumeroAleatório = int.Parse(txtNumeroAleatorio.Text);
+            int campoNumeroAleatório = random.Next(11);
+            txtNumeroAleatorio.Text = campoNumeroAleatório.ToString();
+
+            int resultado = campoNumero + campoNumeroAleatório;
+
+            lblResultado.Text = $"Resultado: {resultado}";
+
+        }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtNumeroAleatorio_TextChanged(object sender, EventArgs e)
+        {
+            txtNumeroAleatorio.Enabled = false;
+            Random random = new Random();
+           // txtNumeroAleatorio.Text = random.Next(11);
+            
+
+        }
+
+        private void txtNumero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            int campoNumero = int.Parse(txtNumero.Text);
+           // int campoNumeroAleatório = int.Parse(txtNumeroAleatorio.Text);
+
+            int campoNumeroAleatório = random.Next(11);
+            txtNumeroAleatorio.Text = campoNumeroAleatório.ToString();
+
+            int resultado = campoNumero - campoNumeroAleatório;
+
+            lblResultado.Text = $"Resultado: {resultado}";
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            int campoNumero = int.Parse(txtNumero.Text);
+            //int campoNumeroAleatório = int.Parse(txtNumeroAleatorio.Text);
+            int campoNumeroAleatório = random.Next(11);
+            txtNumeroAleatorio.Text = campoNumeroAleatório.ToString();
+            int resultado = campoNumero / campoNumeroAleatório;
+
+            lblResultado.Text = $"Resultado: {resultado}";
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            int campoNumero = int.Parse(txtNumero.Text);
+            //int campoNumeroAleatório = int.Parse(txtNumeroAleatorio.Text);
+            int campoNumeroAleatório = random.Next(11);
+            txtNumeroAleatorio.Text = campoNumeroAleatório.ToString();
+            int resultado = campoNumero * campoNumeroAleatório;
+
+            lblResultado.Text = $"Resultado: {resultado}";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
